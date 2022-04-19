75d118c8dba21739bcef15e59ab95baae181b14f
sopoour <sophia.auer@googlemail.com>
2022-04-19 13:17:02 +0200

    implement priority sorting of search results

--- src/hooks/useSearchData.js
+++ src/hooks/useSearchData.js
@@ -21,13 +21,38 @@ const useSearchData = (data, isHome, setIsActive) => {
         results = await data.filter((value) => {
           return (
             //search for either character or actor/actress name
-            value.fields.mainTitle.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            value.fields.mainContent.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            value.fields.keywords.toLowerCase().includes(searchTerm.toLowerCase())
+            value.fields.mainTitle
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase()) ||
+            value.fields.mainContent
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase()) ||
+            value.fields.keywords
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase())
           );
         });
+        const sortedresults = results.sort((a, b) => {
+          const prioA = a.fields.mainTitle
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+            ? 3
+            : a.fields.keywords.toLowerCase().includes(searchTerm.toLowerCase())
+            ? 2
+            : 1;
+          const prioB = b.fields.mainTitle
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+            ? 3
+            : b.fields.keywords.toLowerCase().includes(searchTerm.toLowerCase())
+            ? 2
+            : 1;
+          if (prioA < prioB) return 1;
+          if (prioA > prioB) return -1;
+          return 0;
+        });
         //then the filtered list (results) is set on the searchResults state by using the setSearchResults method
-        setSearchResults(results);
+        setSearchResults(sortedresults);
       }
     })();
   }, [data, searchTerm, isHome]);
