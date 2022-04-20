137bffc5e1535967747be3453bf3a39dba96682b
sopoour <sophia.auer@googlemail.com>
2022-04-20 18:46:47 +0200

    adjust footer legal

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -59,9 +59,14 @@ const Footer = () => {
       <section className={styles.ftLegal}>
         <ul className={styles.ftLegalList}>
           <li>
-            &copy; 2022 Encyclopedia of Ludic Terms. All Rights Reserved.
-            Developed by{" "}
-            <a href="https://sophiaauer.me/" rel="noreferrer" target="_blank" style={{textDecoration: "none", color: "#bbb"}}>
+            &copy; 2022 Encyclopedia of Ludic Terms. All Rights Reserved (unless
+            specified otherwise). Developed by{" "}
+            <a
+              href="https://sophiaauer.me/"
+              rel="noreferrer"
+              target="_blank"
+              style={{ textDecoration: "none", color: "#bbb" }}
+            >
               Sophia Auer
             </a>
             .
