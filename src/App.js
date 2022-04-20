0c8e3b7f596f8bb4a55083ea636e68aee33f7cd7
sopoour <sophia.auer@googlemail.com>
2022-04-20 19:35:57 +0200

    taking lazy load for the rest of the pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { lazy, Suspense } from "react";
-import Home from "./pages/Home";
+/* import Home from "./pages/Home"; */
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import Contents from "./pages/Contents";
+/* import Contents from "./pages/Contents"; */
 import { Route, Routes } from "react-router-dom";
 /* import ContentDetail from "./pages/ContentDetail"; */
-import About from "./pages/About";
+/* import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import AuthorList from "./pages/AuthorList";
 import EditorialInformation from "./pages/EditorialInformation";
 import TableOfContents from "./pages/TableOfContents";
 import HowToCite from "./pages/HowToCite";
-import RecentArticles from "./pages/RecentArticles";
+import RecentArticles from "./pages/RecentArticles"; */
 import styles from "./App.module.css";
 
 const ContentDetail = lazy(() => import("./pages/ContentDetail"));
+const Contents = lazy(() => import("./pages/Contents"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const AuthorList = lazy(() => import("./pages/AuthorList"));
+const EditorialInformation = lazy(() => import("./pages/EditorialInformation"));
+const TableOfContents = lazy(() => import("./pages/TableOfContents"));
+const HowToCite = lazy(() => import("./pages/HowToCite"));
+const RecentArticles = lazy(() => import("./pages/RecentArticles"));
+const Home = lazy(() => import("./pages/Home"));
 
 const App = () => {
   return (
