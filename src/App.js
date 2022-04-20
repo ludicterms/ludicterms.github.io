8f6e567e0ec8dd4dab9605dda911a6e3838aaa73
sopoour <sophia.auer@googlemail.com>
2022-04-20 20:30:32 +0200

    rollback lazy load

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import React, { lazy, Suspense } from "react";
-/* import Home from "./pages/Home"; */
+//import { useState } from "react";
+//import useApiData from "./hooks/useApiData";
+import Home from "./pages/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-/* import Contents from "./pages/Contents"; */
+import Contents from "./pages/Contents";
 import { Route, Routes } from "react-router-dom";
-/* import ContentDetail from "./pages/ContentDetail"; */
-/* import About from "./pages/About";
+import ContentDetail from "./pages/ContentDetail";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import AuthorList from "./pages/AuthorList";
 import EditorialInformation from "./pages/EditorialInformation";
 import TableOfContents from "./pages/TableOfContents";
 import HowToCite from "./pages/HowToCite";
-import RecentArticles from "./pages/RecentArticles"; */
+import RecentArticles from "./pages/RecentArticles";
 import styles from "./App.module.css";
-
-const ContentDetail = lazy(() => import("./pages/ContentDetail"));
-const Contents = lazy(() => import("./pages/Contents"));
-const About = lazy(() => import("./pages/About"));
-const Contact = lazy(() => import("./pages/Contact"));
-const NotFound = lazy(() => import("./pages/NotFound"));
-const AuthorList = lazy(() => import("./pages/AuthorList"));
-const EditorialInformation = lazy(() => import("./pages/EditorialInformation"));
-const TableOfContents = lazy(() => import("./pages/TableOfContents"));
-const HowToCite = lazy(() => import("./pages/HowToCite"));
-const RecentArticles = lazy(() => import("./pages/RecentArticles"));
-const Home = lazy(() => import("./pages/Home"));
 
 const App = () => {
   return (
@@ -33,118 +22,17 @@ const App = () => {
       <NavBar />
       <div className={styles.layout}>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <Home />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/articles"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <Contents />
-              </Suspense>
-            }
-          />
-
-          <Route
-            path="/articles/:contentId"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <ContentDetail />{" "}
-              </Suspense>
-            }
-          />
-
-          <Route
-            path="/about"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <About />{" "}
-              </Suspense>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <Contact />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/author-list"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <AuthorList />{" "}
-              </Suspense>
-            }
-          />
-          <Route
-            path="/editorial-information"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <EditorialInformation />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/toc"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <TableOfContents />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/how-to-cite"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <HowToCite />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/recent-articles"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <RecentArticles />
-              </Suspense>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <Suspense
-                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
-              >
-                <NotFound />
-              </Suspense>
-            }
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<Contents />} />
+          <Route path="/articles/:contentId" element={<ContentDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/author-list" element={<AuthorList />} />
+          <Route path="/editorial-information" element ={<EditorialInformation />} />
+          <Route path="/toc" element ={<TableOfContents />} />
+          <Route path="/how-to-cite" element ={<HowToCite />} />
+          <Route path="/recent-articles" element ={<RecentArticles />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
       <Footer />
