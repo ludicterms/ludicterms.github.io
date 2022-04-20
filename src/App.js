5402c49f35f72f087700690fee9d6d4ef6dbda02
sopoour <sophia.auer@googlemail.com>
2022-04-20 19:21:46 +0200

    another attempt to improve performance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-//import { useState } from "react";
-//import useApiData from "./hooks/useApiData";
+import React, { lazy, Suspense } from "react";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Contents from "./pages/Contents";
 import { Route, Routes } from "react-router-dom";
-import ContentDetail from "./pages/ContentDetail";
+/* import ContentDetail from "./pages/ContentDetail"; */
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
@@ -16,24 +15,33 @@ import HowToCite from "./pages/HowToCite";
 import RecentArticles from "./pages/RecentArticles";
 import styles from "./App.module.css";
 
+const ContentDetail = lazy(() => import("./pages/ContentDetail"));
+
 const App = () => {
   return (
     <div className={styles.layoutWrapper}>
       <NavBar />
       <div className={styles.layout}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/articles" element={<Contents />} />
-          <Route path="/articles/:contentId" element={<ContentDetail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/author-list" element={<AuthorList />} />
-          <Route path="/editorial-information" element ={<EditorialInformation />} />
-          <Route path="/toc" element ={<TableOfContents />} />
-          <Route path="/how-to-cite" element ={<HowToCite />} />
-          <Route path="/recent-articles" element ={<RecentArticles />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/articles" element={<Contents />} />
+
+            <Route path="/articles/:contentId" element={<ContentDetail />} />
+
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/author-list" element={<AuthorList />} />
+            <Route
+              path="/editorial-information"
+              element={<EditorialInformation />}
+            />
+            <Route path="/toc" element={<TableOfContents />} />
+            <Route path="/how-to-cite" element={<HowToCite />} />
+            <Route path="/recent-articles" element={<RecentArticles />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </div>
       <Footer />
     </div>
