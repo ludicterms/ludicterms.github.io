0b6a5337e42df11e97b6e962c2bc57de9df3ce6c
MSDebus <93423803+MSDebus@users.noreply.github.com>
2026-06-09 21:41:47 +0200

    Fixed some potential crawler problem
    Fix semantic HTML structure on Table of Contents page
    
    Restructured layout from invalid <a><li> nesting to valid <ul><li><a> format
    
    Kept identical visual styles using Styled Components
    
    Improved crawlability for search engines/academic robots and accessibility for screen readers

--- src/pages/TableOfContents.js
+++ src/pages/TableOfContents.js
@@ -5,23 +5,31 @@ import { useArticles } from "../hooks/useArticles";
 import styled from "styled-components";
 import SEOHelmet from "../components/SEOHelmet/SEOHelmet";
 
+// Styled List wrapper to replace the generic parent div
+const StyledListWrapper = styled.ul`
+  width: max-content;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
 const StyledLink = styled(Link)`
   text-decoration: none;
   line-height: 35px;
   font-size: 18px;
   color: black;
-  :hover {
+  display: block; /* Ensures the whole line remains comfortably clickable */
+  
+  &:hover {
     color: #006838;
     text-decoration: underline;
-  }
-  li {
-    list-style: none;
   }
 `;
 
 const TableOfContents = () => {
   const [articles, loading] = useArticles();
-  const sortedArticles = articles.sort((a, b) => {
+  
+  const sortedArticles = [...articles].sort((a, b) => {
     if (a.fields.mainTitle < b.fields.mainTitle) {
       return -1;
     }
@@ -42,15 +50,20 @@ const TableOfContents = () => {
         <div className="pageTitle">
           <h1>Table of Contents</h1>
         </div>
-        <div style={{ width: "max-content" }}>
+        
+        {/* Changed from a <div> to a semantic <ul> element */}
+        <StyledListWrapper>
           {!loading &&
             sortedArticles.length > 0 &&
             sortedArticles.map((item) => (
-              <StyledLink to={`../articles/${item.fields.slug}`}>
-                <li key={item.fields.slug}>{item.fields.mainTitle}</li>
-              </StyledLink>
+              /* Correct HTML Syntax: <ul> -> <li> -> <a> */
+              <li key={item.fields.slug}>
+                <StyledLink to={`../articles/${item.fields.slug}`}>
+                  {item.fields.mainTitle}
+                </StyledLink>
+              </li>
             ))}
-        </div>
+        </StyledListWrapper>
       </Section>
     </React.Fragment>
   );
