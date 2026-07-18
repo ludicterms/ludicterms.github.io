81bfeca8e4c997a649344211f54fa781612abe98
MSDebus <93423803+MSDebus@users.noreply.github.com>
2026-07-18 09:46:42 +0200

    Render markdown in article card abstracts
    - Import Markdown from react-markdown in Card component
    - Replace plain text <p> with <Markdown> inside a <div>
      to avoid invalid nested <p> elements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Markdown from "react-markdown";
 import styles from "./Card.module.css";
 
 const Card = (props) => {
@@ -13,7 +14,7 @@ const Card = (props) => {
     >
       <div className={styles.card}>
         <h1>{props.data.fields.mainTitle}</h1>
-        <p className={styles.preview}> {props.data.fields.abstract}</p>
+        <div className={styles.preview}><Markdown children={props.data.fields.abstract} /></div>
         <p>Author: {props.data.fields.author}</p>
       </div>
     </Link>
